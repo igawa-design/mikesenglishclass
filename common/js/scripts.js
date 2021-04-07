72cf02a88f4499adbdb8ceaf6ea18cf0076db487
igawa <80309695+igawa-design@users.noreply.github.com>
2021-04-07 17:44:54 +0900

    update copyright code

--- common/js/scripts.js
+++ common/js/scripts.js
@@ -252,7 +252,13 @@ let magicGrid = new MagicGrid({
   container: "#container", // Required. Can be a class, id, or an HTMLElement.
   static: true, // Required for static content.
   animate: true, // Optional.
-  gutter: 0, // Optional. Space between items. Default: 25(px).
+  gutter: 0, // Optional. Space between items. Default: 25(px). *change 25 to 0, applying a css.
 });
 
 magicGrid.listen();
+
+// Copyright //////////////////////////////////////////////////////
+
+var date = new Date();
+  $('.copyright').text(
+    date.getFullYear());

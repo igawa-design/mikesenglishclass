9375b999da7c1b691a1ef987a343fe8908dc16f9
igawa <80309695+igawa-design@users.noreply.github.com>
2021-04-12 12:28:39 +0900

    update footer animation

--- common/js/scripts.js
+++ common/js/scripts.js
@@ -201,7 +201,7 @@ $(window).scroll(function () {
 // scroll add class scale  /////////////////////////////////////////////////////////////
 
 $(window).scroll(function (){
-  $(".sec_icon, .sec_img, .cloud").each(function(){
+  $(".sec_icon, .sec_img, .info_h2_logo").each(function(){
     var hit		= $(this).offset().top;
     var scroll	= $(window).scrollTop();
     var wHeight	= $(window).height();
@@ -217,7 +217,7 @@ $(window).scroll(function (){
 // scroll add class fade   /////////////////////////////////////////////////////////////
 
 $(window).scroll(function (){
-  $(".section, .sec_box, .info, .nav_local").each(function(){
+  $(".section, .sec_box, .nav_local, .sincerely, .info_h3, .info_ul, .info_a_sitemap").each(function(){
     var hit		= $(this).offset().top;
     var scroll	= $(window).scrollTop();
     var wHeight	= $(window).height();

8bb733807e851f29e7ab67b60769a71dd363ea94
igawa <80309695+igawa-design@users.noreply.github.com>
2021-04-15 08:59:22 +0900

    add remove action

--- functions.php
+++ functions.php
@@ -7,6 +7,9 @@ remove_action('wp_head', 'rsd_link');
 remove_action('wp_head', 'wp_shortlink_wp_head');
 remove_action('wp_head', 'feed_links_extra', 3);
 remove_action('wp_head', 'adjacent_posts_rel_link_wp_head');
+remove_action('wp_head','rest_output_link_wp_head');
+remove_action('wp_head','wp_oembed_add_discovery_links');
+remove_action('wp_head','wp_oembed_add_host_js');
 add_filter( 'wp_resource_hints', 'remove_dns_prefetch', 10, 2 );
 function remove_dns_prefetch( $hints, $relation_type ) {
 	if ( 'dns-prefetch' === $relation_type ) {

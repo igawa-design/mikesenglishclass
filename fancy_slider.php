c6c4099618e26f04c25066e7a346af926010a7dd
igawa <80309695+igawa-design@users.noreply.github.com>
2021-05-26 12:42:07 +0900

    05.26 12:40

--- fancy_slider.php
+++ fancy_slider.php
@@ -18,7 +18,7 @@
 <span class="fnc_txt first_txt">Mike’s English Class Sapporo</span>
 </span>
 </h2>
-<a class="fnc_slide_link" href="aboutmike.html">more...</a>
+<a class="fnc_slide_link" href="<?php echo home_url('aboutmike'); ?>">more...</a>
 </div><!-- slide_01 -->
 </div><!-- fnc-slide__content -->
 </div><!-- fnc-slide__inner -->
@@ -39,7 +39,7 @@
 <span class="fnc_txt first_txt">One to One</span>
 </span>
 </h2>
-<a class="fnc_slide_link" href="one-to-one.html">more...</a>
+<a class="fnc_slide_link" href="<?php echo home_url('one-to-one'); ?>">more...</a>
 </div><!-- slide_02 -->
 </div><!-- fnc-slide__content -->
 </div><!-- fnc-slide__inner -->
@@ -61,7 +61,7 @@
 <span class="fnc_txt first_txt">Location</span>
 </span>
 </h2>
-<a class="fnc_slide_link" href="location.html">more...</a>
+<a class="fnc_slide_link" href="<?php echo home_url('location'); ?>">more...</a>
 </div><!-- slide_03 -->
 </div><!-- fnc-slide__content -->
 </div><!-- fnc-slide__inner -->
@@ -153,7 +153,7 @@ Contact
 <h3 class="fnc-cont__credits-name">マイク英会話教室札幌</h3>
 <a href="https://www.facebook.com/mikesenglishclass" class="fnc-cont__credits-link"><img class="fnc_icon facebook" alt="英会話＆英語スクール マイク英会話教室札幌のfacebookはこちら。" width="25" height="25" loading="lazy" src="common/img/icon_facebook_blue.webp">Facebook</a>
 <a href="https://www.youtube.com/watch?v=o5a8qeqvxX0" class="fnc-cont__credits-link"><img class="fnc_icon youtube" alt="英会話＆英語スクール マイク英会話教室札幌のYouTube" width="25" height="25" loading="lazy" src="common/img/icon_youtube_red.svg">YouTube</a>
-<a href="contact.html" class="fnc-cont__credits-link"><img class="fnc_icon contact" alt="英会話＆英語スクール マイク英会話教室札幌のfacebookはこちら。" width="25" height="25" loading="lazy" src="common/img/icon_contact.svg">Contact</a>
+<a href="<?php echo home_url('contact'); ?>" class="fnc-cont__credits-link"><img class="fnc_icon contact" alt="英会話＆英語スクール マイク英会話教室札幌のfacebookはこちら。" width="25" height="25" loading="lazy" src="common/img/icon_contact.svg">Contact</a>
 <p class="fnc-cont__credits_txt">マイク英会話教室札幌に興味をお持ちいただき、誠にありがとうございます。</p>
 <p class="fnc-cont__credits_txt">ご意見・ご質問・ご相談などお気軽にお問い合わせください。<br>
 ご連絡は英語でも日本語でも大丈夫です。</p>
--- page.php
+++ page.php
@@ -2,30 +2,7 @@
 Template Name: page
 */ ?>
 
-<?php get_header(); ?>
-
-<body id="PAGE">
-
-<header id="header">
-<div class="flex">
-<h1><a href="<?php echo home_url(); ?>"><img alt="英会話＆英語教室 マイク英会話教室札幌" src="<?php echo get_template_directory_uri(); ?>/common/img/logo.png"></a></h1>
-<div class="box facebook">
-<a href="https://www.facebook.com/mikesenglishclass" target="_blank">
-<img alt="マイク英会話教室札幌 facebook" src="<?php echo get_template_directory_uri(); ?>/common/img/icon_facebook.svg" class="icon_facebook"></a>
-</div><!--box-->
-<div class="box youtube">
-<a href="https://www.youtube.com/channel/UCxV2V8PO1QbFdeQOODeeyXw" target="_blank">
-<img alt="
-マイク英会話教室札幌 YouTube" src="<?php echo get_template_directory_uri(); ?>/common/img/icon_youtube.svg" class="icon_youtube"></a>
-</div><!--box-->
-</div><!--flex-->
-
-<?php get_template_part('nav'); ?>
-
-</header>
-
-<main>
-<section>
+<?php get_header('page'); ?>
 
 <?php if (have_posts()) : ?>
 <?php while (have_posts()) : the_post(); ?>
@@ -39,10 +16,7 @@ Template Name: page
 
 <?php else : ?>
 <h2 id="not_found"><em>ページが見つかりません。</em><span lang="en">Not Found</span></h2>
-<p><a href="<?php echo home_url(); ?>"><span lang="en">Back To HOME</span> - TOPページへ戻る</a></p>
+<p><a href="<?php echo home_url(); ?>"><span lang="en">Back To HOME</span> - HOMEへ戻る</a></p>
 <?php endif; ?>
-
-</section>
-</main>
 
 <?php get_footer(); ?>

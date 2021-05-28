4a245720f375d40e5a43fe9462462029bdbbd26f
igawa <80309695+igawa-design@users.noreply.github.com>
2021-05-29 00:38:12 +0900

    change sliding delay

--- common/js/fancy_slider.js
+++ common/js/fancy_slider.js
@@ -166,7 +166,7 @@ autoSliding - boolean
 autoSlidingDelay - delay in ms. If audoSliding is on and no value provided, default value is 5000
 blockASafterClick - boolean. If user clicked any sliding control, autosliding won't start again
 */
-fncSlider(".fancy-slider", {autoSlidingDelay: 8000});
+fncSlider(".fancy-slider", {autoSlidingDelay: 4000});
 
 var $demoCont = document.querySelector(".fnc-cont");
 

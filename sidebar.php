13a366a928a6f96fc6e33c84db8650131efa8024
igawa <80309695+igawa-design@users.noreply.github.com>
2021-06-04 02:37:01 +0900

    06.04

--- sidebar.php
+++ sidebar.php
@@ -1,26 +1,42 @@
 <div id="sidebar">
 
-<section id="CATEGORIES" class="SECTION">
+<section id="csategories" class="section">
+<div class="sec_box sec_box_reviews">
+<div class="sec_box_inner">
 <h2>Categories</h2>
 <ul>
 <?php wp_list_categories('title_li='); ?>
 </ul>
-</section><!--CATEGORIES SECTION-->
+</div><!--sec_box_inner-->
+</div><!--sec_box sec_box_reviews-->
+</section><!--categories section-->
 
-<section id="TAGS" class="SECTION">
+<section id="tags" class="section">
+<div class="sec_box sec_box_reviews">
+<div class="sec_box_inner">
 <h2>Tags</h2>
 <p><?php wp_tag_cloud('smallest=6 & largest=14'); ?></p>
-</section><!--TAGS SECTION-->
+</div><!--sec_box_inner-->
+</div><!--sec_box sec_box_reviews-->
+</section><!--tags section-->
 
-<section id="SEARCH" class="SECTION">
+<div id="search" class="section">
+<div class="sec_box sec_box_reviews">
+<div class="sec_box_inner">
 <?php get_search_form(); ?>
-</section><!--SEARCH SECTION-->
+</div><!--sec_box_inner-->
+</div><!--sec_box sec_box_reviews-->
+</div><!--search section-->
 
-<section id="ARCHIVES" class="SECTION">
+<section id="archives" class="section">
+<div class="sec_box sec_box_reviews">
+<div class="sec_box_inner">
 <h2>Archives</h2>
 <ul>
 <?php wp_get_archives('type=monthly&post_type=post'); ?>
 </ul>
-</section><!--ARCHIVES SECTION-->
+</div><!--sec_box_inner-->
+</div><!--sec_box sec_box_reviews-->
+</section><!--archives section-->
 
 </div><!--sidebar-->

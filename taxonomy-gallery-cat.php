53fbc93fb72d2b8df765c99f3a5030cfb07a050b
igawa <80309695+igawa-design@users.noreply.github.com>
2021-06-19 14:17:45 +0900

    06.19 reviews, gallery and h tags design

--- taxonomy-gallery-cat.php
+++ taxonomy-gallery-cat.php
@@ -6,7 +6,7 @@ Template Name: taxonomy-gallery-cat
 
 <main>
 <figure class="fig main_view">
-<figcaption class="figcaption">Gallery</figcaption>
+<figcaption class="figcaption">taxonomy-gallery-cat</figcaption>
 <picture>
 	<source media="(max-width: 767px)" srcset="https://igawa.co/mikesenglishclass/wp-content/themes/mikesenglishclass/common/img/GALLERY/gallery_1400x1050.jpg">
 	<source media="(min-width: 768px)" srcset="https://igawa.co/mikesenglishclass/wp-content/themes/mikesenglishclass/common/img/GALLERY/gallery_1920x1440.jpg">
@@ -14,15 +14,63 @@ Template Name: taxonomy-gallery-cat
 	</picture>
 </figure>
 
+<?php
+$now_id = $cat_now->cat_ID;
+$now_slug = $cat_now->slug;
+$now_name = $cat_now->cat_name;
+$taxonomy = $wp_query->get_queried_object();
+
+$term = array_pop(get_the_terms($post->ID, 'gallery-cat'));
+$term_p = $term->parent;
+if ( ! $term_p == 0 ){
+$term = array_shift(get_the_terms($post->ID, 'gallery-cat'));
+}
+?>
+
 <section class="section w100">
-<h2 class="sec_h2">ギャラリー 一覧</h2>
+<h2 class="sec_h2 sec_h2_gallery"><em><?php echo esc_html($term->name); ?>ギャラリー</em><span><?php echo esc_html($term->slug); ?> gallery</span></h2>
 <p class="sec_txt_lead gallery_lead">Photo Gallery</p>
-<h3 class="sec_h3 sec_h3_02"><img class="sec_icon" alt="マイク英会話教室札幌のギャラリー一覧" width="50" height="50" loading="lazy" src="https://igawa.co/mikesenglishclass/wp-content/themes/mikesenglishclass/common/img/icon_sec_gallery.svg"></h3>
+<h3 class="sec_h3 sec_h3_02"><img class="sec_icon" alt="マイク英会話教室札幌のギャラリー" width="50" height="50" loading="lazy" src="https://igawa.co/mikesenglishclass/wp-content/themes/mikesenglishclass/common/img/icon_sec_gallery.svg"></h3>
 </section><!-- section w100 -->
 
 <div class="archive section">
-<div class="sec_box sec_box_garelly">
+<div class="sec_box sec_box_gallery">
 <div class="sec_box_inner">
+
+<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+
+<article>
+<h4 class="article_h4"><?php echo get_the_title(); ?></h4>
+<time datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('Y.m.d'); ?></time>
+<a href="<?php the_permalink(); ?>">
+<div class="post">
+<?php the_post_thumbnail(); ?>
+<?php the_excerpt(); ?>
+</div><!--post-->
+</a>
+<small class="article_cat">
+<?php
+$terms = get_the_terms($post->ID,'gallery-cat');
+foreach( $terms as $term ) {
+echo '<a href="'.get_term_link($term->slug, 'gallery-cat').'" class="cat">'.$term->name.'</a>';
+}
+?>
+</small>
+</article>
+
+<?php endwhile; ?>
+
+<?php if(function_exists('wp_pagenavi')) { wp_pagenavi(); } ?>
+
+</div><!--sec_box_inner-->
+</div><!--sec_box sec_box_gallery-->
+</div><!--archive section-->
+
+<?php get_sidebar('gallery'); ?>
+
+</main>
+
+<?php get_footer(); ?>
 
 
 

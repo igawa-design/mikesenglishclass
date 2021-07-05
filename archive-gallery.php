56ecb25e52212af7c1bd832c618eb7253f12c5f6
igawa <80309695+igawa-design@users.noreply.github.com>
2021-07-05 17:59:02 +0900

    add text for seo

--- archive-gallery.php
+++ archive-gallery.php
@@ -8,9 +8,9 @@ Template Name: archive-gallery
 <figure class="fig main_view">
 <figcaption class="figcaption">archive-gallery</figcaption>
 <picture>
-<source media="(max-width: 767px)" srcset="https://igawa.co/mikesenglishclass/wp-content/themes/mikesenglishclass/common/img/GALLERY/gallery_1400x1050.jpg">
-<source media="(min-width: 768px)" srcset="https://igawa.co/mikesenglishclass/wp-content/themes/mikesenglishclass/common/img/GALLERY/gallery_1920x1440.jpg">
-<img alt="英会話スクールでの生徒さんとの写真。マンツーマンでの英語・英会話のコーチングをしています。" width="1400" height="1050" loading="lazy" src="https://igawa.co/mikesenglishclass/wp-content/themes/mikesenglishclass/common/img/GALLERY/gallery_1400x1050.jpg">
+<source media="(max-width: 767px)" srcset="<?php echo get_template_directory_uri(); ?>/common/img/GALLERY/gallery_1400x1050.jpg">
+<source media="(min-width: 768px)" srcset="<?php echo get_template_directory_uri(); ?>/common/img/GALLERY/gallery_1920x1440.jpg">
+<img alt="英会話スクールの生徒さんたちと教師マイク先生との写真。マンツーマンでの英語・英会話のコーチングをしています。" width="1400" height="1050" loading="lazy" src="<?php echo get_template_directory_uri(); ?>/common/img/GALLERY/gallery_1400x1050.jpg">
 </picture>
 </figure>
 
@@ -18,7 +18,7 @@ Template Name: archive-gallery
 <h2 class="sec_h2 sec_h2_gallery"><?php echo esc_html(get_post_type_object(get_post_type())->label); ?></h2>
 <p class="sec_txt_lead gallery_lead">Photo Gallery</p>
 <h3 class="sec_h3 sec_h3_02">
-<a href="<?php echo home_url('gallery'); ?>"><img class="sec_icon" alt="マイク英会話教室札幌のギャラリー" width="50" height="50" loading="lazy" src="https://igawa.co/mikesenglishclass/wp-content/themes/mikesenglishclass/common/img/icon_sec_gallery.svg"></a>
+<a href="<?php echo home_url('gallery'); ?>"><img class="sec_icon" alt="生徒さんたちと教師マイク先生との写真。マイク英会話教室札幌のギャラリー" width="50" height="50" loading="lazy" src="<?php echo get_template_directory_uri(); ?>/common/img/icon_sec_gallery.svg"></a>
 </h3>
 </section><!-- section w100 -->
 

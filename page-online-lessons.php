8ab7d823afe839f64733fc64df6af67c64396662
igawa <80309695+igawa-design@users.noreply.github.com>
2021-09-07 17:57:28 +0900

    09.07 modified online lessons

--- page-online-lessons.php
+++ page-online-lessons.php
@@ -46,7 +46,7 @@ Template Name: page-online-lessons
 <video src="<?php echo get_stylesheet_directory_uri(); ?>/online-lessons/movie/01_online_lessons_m.mp4" autoplay muted playsinline preload="none"></video>
 <video src="<?php echo get_stylesheet_directory_uri(); ?>/online-lessons/movie/02_online_lessons_m.mp4" autoplay muted playsinline preload="none"></video>
 
-<p>2013年の開校以来、オンラインレッスンをご希望の方へは、SkypeやFaceTimeなどを利用し自宅や職場からのレッスンへ対応しておりました。</p>
+<p>2013年の開校以来、オンライン個人レッスンをご希望の方には、SkypeやFaceTimeなどで自宅や職場からのレッスンにも対応しておりました。</p>
 
 </div><!--sec_box_inner-->
 </div><!--sec_box sec_box_online-->
